788e2326df5800e62707a35feb07b0cebcbbfc34
PioneerX7 <vita-kopy@yandex.ru>
2022-12-08 09:46:10 +0300

    все задачи

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         task1();
-
+        task2();
         task3();
+        task4();
+        task5();
     }
 
     public static void task1() {
@@ -51,17 +53,68 @@ public class Main {
     public static void task3() {
         System.out.println("Задача 3");
 
-        int year = 2008;
+        int year = 2400;
         int bisYear = year % 4;
         int bisYear100 = year % 100;
         int bisYear400 = year % 400;
 
         if (bisYear == 0) {
-            if (){
-
+            if (bisYear100 == 0 && bisYear400 != 0) {
+                System.out.println(year + " год не является високосным.");
+            } else {
+                System.out.println(year + " год является високосным");
             }
+        } else {
+            System.out.println(year + " год не является високосным.");
         }
+    }
+
+    public static void task4() {
+        System.out.println("Задача 4");
+
+        int deliveryDistance = 95;
+
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется дней: " + 1);
+        } else if (deliveryDistance > 20 && deliveryDistance < 60) {
+            System.out.println("Потербуется дней: " + 2);
+        } else if (deliveryDistance > 60 && deliveryDistance < 100) {
+            System.out.println("Потребуется дней: " + 3);
+        } else {
+            System.out.println("Доставка не осуществляется.");
+        }
+    }
+
+    public static void task5() {
+        System.out.println("Задача 5");
+
+        int month = 1;
+
+        switch (month) {
+            case (1):
+            case (2):
+            case (12):
+                System.out.println(month + " месяц принадлежит к сезону зима");
+                break;
+            case (3):
+            case (4):
+            case (5):
+                System.out.println(month + " месяц принадлежит к сезону весна");
+                break;
+            case (6):
+            case (7):
+            case (8):
+                System.out.println(month + " месяц принадлежит к сезону лето");
+                break;
+            case (9):
+            case (10):
+            case (11):
+                System.out.println(month + " месяц принадлежит к сезону осень");
+                break;
+            default:
+                System.out.println("В году всего 12 месяцев");
 
 
+        }
     }
 }
